e88c34b345366279e8a8a219c190cdd45f3210aa
João Pinheiro <joao.castropinheiro@gmail.com>
2023-06-25 20:59:34 +0100

    styling

--- lib/abra_web/live/abra_live.ex
+++ lib/abra_web/live/abra_live.ex
@@ -11,15 +11,13 @@ defmodule AbraWeb.AbraLive do
     # TODO: since this is a game, we should update the client highlight pos
     # immediately & only then send to the server.
     ~H"""
-    <div class="font-mono flex justify-center">
+    <div class="font-mono flex">
       <%= for {c, i} <- Enum.with_index(String.graphemes((@text))) do %>
         <.live_component module={AbraWeb.AbraLiveComponent} id={i} c={c} active={i == @pos} />
       <% end %>
     </div>
-    <div class="font-mono flex justify-evenly">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" phx-click="up">+</button>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" phx-click="down">-</button>
-    </div>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" phx-click="up">+</button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" phx-click="down">-</button>
     """
   end
 
