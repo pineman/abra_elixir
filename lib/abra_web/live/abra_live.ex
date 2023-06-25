62fc2906afff1f525acd40d90d7bbb03ad0192f7
João Pinheiro <joao.castropinheiro@gmail.com>
2023-06-25 22:35:28 +0100

    try temporary assigns

--- lib/abra_web/live/abra_live.ex
+++ lib/abra_web/live/abra_live.ex
@@ -1,29 +1,33 @@
 defmodule AbraWeb.AbraLive do
   use AbraWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, pos: 0, text: "This is a test string")}
+  defmodule Char do
+    defstruct [:i, :c]
   end
 
-  defmodule CharComponent do
-    use AbraWeb, :live_component
+  def mount(_params, _session, socket) do
+    text = "This is a test string"
 
-    def render(assigns) do
-      ~H"""
-      <span class={if @active, do: "bg-red-400"}>
-        <%= if @c == " ", do: raw("&nbsp;"), else: @c %>
-      </span>
-      """
-    end
+    chars =
+      text
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.map(fn {c, i} ->
+        %Char{i: i, c: c}
+      end)
+
+    {:ok, assign(socket, pos: 0, text: text, chars: chars), temporary_assigns: [chars: []]}
   end
 
   def render(assigns) do
     # TODO: since this is a game, we should update the client highlight pos
     # immediately & only then send to the server.
     ~H"""
-    <div class="font-mono flex">
-      <%= for {c, i} <- Enum.with_index(String.graphemes((@text))) do %>
-        <.live_component module={CharComponent} id={i} c={c} active={i == @pos} />
+    <div class="font-mono flex" id="text" phx-update="append">
+      <%= for char <- @chars do %>
+        <span id={"#{char.i}"} class={if char.i == @pos, do: "bg-red-400"}>
+          <%= if char.c == " ", do: raw("&nbsp;"), else: char.c %>
+        </span>
       <% end %>
     </div>
     <button
@@ -42,12 +46,26 @@ defmodule AbraWeb.AbraLive do
   end
 
   def handle_event("up", _unsigned_params, socket) do
-    socket = update(socket, :pos, &(&1 + 1))
+    %{assigns: %{pos: pos, text: text}} = socket
+
+    chars = [
+      %Char{i: pos, c: String.at(text, pos)},
+      %Char{i: pos + 1, c: String.at(text, pos + 1)}
+    ]
+
+    socket = assign(socket, pos: pos + 1, chars: chars)
     {:noreply, socket}
   end
 
   def handle_event("down", _unsigned_params, socket) do
-    socket = update(socket, :pos, &(&1 - 1))
+    %{assigns: %{pos: pos, text: text}} = socket
+
+    chars = [
+      %Char{i: pos, c: String.at(text, pos)},
+      %Char{i: pos - 1, c: String.at(text, pos - 1)}
+    ]
+
+    socket = assign(socket, pos: pos - 1, chars: chars)
     {:noreply, socket}
   end
 end
