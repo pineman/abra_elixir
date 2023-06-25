428e5ee0409825f0f8738dbd469cec060e507bd5
João Pinheiro <joao.castropinheiro@gmail.com>
2023-06-25 21:06:43 +0100

    cleanup

--- lib/abra_web/live/abra_live.ex
+++ lib/abra_web/live/abra_live.ex
@@ -2,18 +2,28 @@ defmodule AbraWeb.AbraLive do
   use AbraWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, pos: 10, text: "This is a test string")}
+    {:ok, assign(socket, pos: 0, text: "This is a test string")}
+  end
+
+  defmodule CharComponent do
+    use AbraWeb, :live_component
+
+    def render(assigns) do
+      ~H"""
+      <span class={if @active, do: "bg-red-400"}>
+        <%= if @c == " ", do: raw("&nbsp;"), else: @c %>
+      </span>
+      """
+    end
   end
 
   def render(assigns) do
-    # TODO: this is sending ALL spans on an update.
-    # figure out why / a better way
     # TODO: since this is a game, we should update the client highlight pos
     # immediately & only then send to the server.
     ~H"""
     <div class="font-mono flex">
       <%= for {c, i} <- Enum.with_index(String.graphemes((@text))) do %>
-        <.live_component module={AbraWeb.AbraLiveComponent} id={i} c={c} active={i == @pos} />
+        <.live_component module={CharComponent} id={i} c={c} active={i == @pos} />
       <% end %>
     </div>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" phx-click="up">+</button>
